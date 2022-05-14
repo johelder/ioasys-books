f48455228c12fdf6d3732b82186e9af04b065f63
johelder <johelder.araujo@pede.ai>
2022-05-14 14:31:21 -0300

    feat: create app theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+  colors: {
+    primary: '#B22E6F',
+    secondary: '#AB2680',
+    label: '#F2F2F2',
+    light: '#FFFFFF',
+    dark: '#333333',
+
+    overlay_secondary: 'rgba(0, 0, 0, 0.3)',
+  },
+  fonts: {
+    primary_300: 'Heebo-Light',
+    primary_400: 'Heebo-Regular',
+    primary_500: 'Heebo-Medium',
+  },
+};
