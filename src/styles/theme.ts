1be553c0235dcaa4de7f8a6efc6c913a97942b1c
johelder <johelder.araujo@pede.ai>
2022-05-17 03:15:01 -0300

    feat: adding new colors to theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -6,8 +6,11 @@ export const theme = {
     light: '#FFFFFF',
     dark: '#333333',
     attention: '#177FBF',
+    light_gray: '#999999',
 
     overlay_secondary: 'rgba(0, 0, 0, 0.3)',
+    border: 'rgba(51, 51, 51, 0.2)',
+    placeholder: 'rgba(153, 153, 153, 0.7)',
   },
   fonts: {
     primary_300: 'Heebo-Light',
