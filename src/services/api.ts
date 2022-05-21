ab360c6c39e48e297a24f13ca07e6b4accbd4ad9
johelder <johelder.araujo@pede.ai>
2022-05-21 05:33:55 -0300

    feat: create api interceptors to get new refresh-token

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,3 +8,28 @@ export const api = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+api.interceptors.response.use(
+  response => response,
+  async error => {
+    if (axios.isCancel(error)) {
+      return Promise.reject(error);
+    }
+
+    if (error.response.status === 401 && !error.config.data) {
+      const response = await api.post('/auth/refresh-token', {
+        refreshToken: error.config.headers.Authorization,
+      });
+
+      const {
+        Authorization: newAuthorization,
+        ['refresh-token']: newRefreshToken,
+      } = response.data;
+
+      error.config.headers.Authorization = newAuthorization;
+      error.config.headers['refresh-token'] = newRefreshToken;
+
+      return Promise.resolve(response);
+    }
+  },
+);
