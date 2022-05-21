2a7539f20c8a615e4dd7df1e215aef2448b5c3ef
johelder <johelder.araujo@pede.ai>
2022-05-21 05:20:59 -0300

    feat: adding book details page to auth stack

--- src/routes/auth.stack.routes.tsx
+++ src/routes/auth.stack.routes.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import {Home} from '../pages';
+import {Home, BookDetails} from '../pages';
 
-const Stack = createNativeStackNavigator();
+export type RootStackParamList = {
+  Home: undefined;
+  BookDetails: {bookId: string};
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const StackAuthRoutes = () => {
   return (
@@ -12,6 +17,7 @@ export const StackAuthRoutes = () => {
       screenOptions={{headerShown: false}}
       initialRouteName="Home">
       <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="BookDetails" component={BookDetails} />
     </Stack.Navigator>
   );
 };
