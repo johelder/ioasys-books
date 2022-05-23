75ab515d2467ce67a65f7c33d5c6e835be61b32d
johelder <johelder.araujo@pede.ai>
2022-05-22 22:38:10 -0300

    feat: adding filter and button modal to index components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 import {Input} from './Input';
 import {ButtonIcon} from './ButtonIcon';
 import {BookCard} from './BookCard';
+import {FilterModal} from './FilterModal';
+import {FilterButton} from './FIlterButton';
 
-export {Input, ButtonIcon, BookCard};
+export {Input, ButtonIcon, BookCard, FilterModal, FilterButton};
