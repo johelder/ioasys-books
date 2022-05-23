a19f909e9f4075d4e06978e31e1a579a9de8d2d1
johelder <johelder.araujo@pede.ai>
2022-05-22 22:42:36 -0300

    feat: using filter provider

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,10 +1,15 @@
 import React, {ReactNode} from 'react';
 import {AuthProvider} from './auth';
+import {FilterProvider} from './filter';
 
 interface IAppProviderProps {
   children: ReactNode;
 }
 
 export const AppProvider = ({children}: IAppProviderProps) => {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <AuthProvider>
+      <FilterProvider>{children}</FilterProvider>
+    </AuthProvider>
+  );
 };
