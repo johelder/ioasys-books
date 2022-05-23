9221464add503820383f479a029ef1c5e193d645
johelder <johelder.araujo@pede.ai>
2022-05-22 22:39:22 -0300

    feat: create filters data

---
+++ src/components/FilterModal/data.ts
@@ -0,0 +1,131 @@
+import uuid from 'react-native-uuid';
+import {IFilter} from '../../dtos';
+
+export const categories: IFilter[] = [
+  {
+    id: uuid.v4(),
+    name: 'Biografias',
+  },
+
+  {
+    id: uuid.v4(),
+    name: 'Coleções',
+  },
+
+  {
+    id: uuid.v4(),
+    name: 'Comportamento',
+  },
+
+  {
+    id: uuid.v4(),
+    name: 'Contos',
+  },
+
+  {
+    id: uuid.v4(),
+    name: 'Crítica Literária',
+  },
+
+  {
+    id: uuid.v4(),
+    name: 'Ficção Científica',
+  },
+
+  {
+    id: uuid.v4(),
+    name: 'Folclore',
+  },
+
+  {
+    id: uuid.v4(),
+    name: 'Genealogia',
+  },
+
+  {
+    id: uuid.v4(),
+    name: 'Humor',
+  },
+
+  {
+    id: uuid.v4(),
+    name: 'Jogos',
+  },
+
+  {
+    id: uuid.v4(),
+    name: 'Jornais',
+  },
+
+  {
+    id: uuid.v4(),
+    name: 'Crianças',
+  },
+
+  {
+    id: uuid.v4(),
+    name: 'Literatura Brasileira',
+  },
+
+  {
+    id: uuid.v4(),
+    name: 'Literatura Estrangeira',
+  },
+
+  {
+    id: uuid.v4(),
+    name: 'Livros Raros',
+  },
+
+  {
+    id: uuid.v4(),
+    name: 'Manuscritos',
+  },
+
+  {
+    id: uuid.v4(),
+    name: 'Poesia',
+  },
+
+  {
+    id: uuid.v4(),
+    name: 'Outros Assuntos',
+  },
+];
+
+export const years: IFilter[] = [
+  {
+    id: uuid.v4(),
+    name: '2015',
+  },
+
+  {
+    id: uuid.v4(),
+    name: '2016',
+  },
+
+  {
+    id: uuid.v4(),
+    name: '2017',
+  },
+
+  {
+    id: uuid.v4(),
+    name: '2018',
+  },
+
+  {
+    id: uuid.v4(),
+    name: '2019',
+  },
+
+  {
+    id: uuid.v4(),
+    name: '2020',
+  },
+
+  {
+    id: uuid.v4(),
+    name: '2021',
+  },
+];
