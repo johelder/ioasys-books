ea00e2058c9204bb78b0dadc08a90f2a998dfe32
johelder <johelder.araujo@pede.ai>
2022-05-22 22:39:59 -0300

    feat: adding filter dto to index

--- src/dtos/index.ts
+++ src/dtos/index.ts
@@ -1,5 +1,6 @@
 import {ISignInData} from './SignInDTO';
 import {IBookData} from './BookDTO';
 import {TPageStatus} from './PageStatusDTO';
+import {IFilter} from './FilterDTO';
 
-export type {ISignInData, IBookData, TPageStatus};
+export type {ISignInData, IBookData, TPageStatus, IFilter};
